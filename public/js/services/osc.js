7148eda83e85b1584fbb25a8a09ec08f64a8c4bc
Anton Koch <anton.koch@gmail.com>
2017-03-01 12:24:57 +0100

    split hostname to remove port for websockets

--- public/js/services/osc.js
+++ public/js/services/osc.js
@@ -5,7 +5,7 @@ define(['angular'], function () {
     return angular.module('woc.services.osc', []).
         factory('oscService', function () {
             var port = new osc.WebSocketPort({
-                url: "ws://" + location.host + ":8081"
+                url: "ws://" + location.host.split(':')[0] + ":8081"
             });
             port.open();
             return {
